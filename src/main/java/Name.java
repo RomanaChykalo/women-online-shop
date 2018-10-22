47a7c9ac109af0e78273311f9d372f7a80f5f37b
FuleiNataliya <fulei.nataliya@gmail.com>
2018-10-22 23:05:15 +0300

    added name

---
+++ src/main/java/Name.java
@@ -0,0 +1,27 @@
+public class Name {
+    private String firstName;
+    private String middleName;
+    private String lastName;
+
+    Name(String firstName, String middleName, String lastName) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+    }
+
+    public String toString() {
+        return "User name is " + firstName + " " + middleName + " " + lastName + "";
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
