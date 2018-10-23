3a8c9f095e2437fbfae2321ae25534415e40ccf4
Romana Chykalo <romo4kachukalo@gmail.com>
2018-10-23 19:25:50 +0300

    changed some names

--- src/main/java/com/women/online/shop/CategoryNotFoundException.java
+++ src/main/java/com/women/online/shop/CategoryNotFoundException.java
@@ -1,7 +1,7 @@
 package taskLesson;
 
 public class CategoryNotFoundException extends RuntimeException {
-	public CategoryNotFoundException(String e) {
-		super(e);
+	public CategoryNotFoundException(String textException) {
+		super(textException);
 	}
 }
