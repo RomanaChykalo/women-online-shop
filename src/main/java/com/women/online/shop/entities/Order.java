9cade262dbc8fb8e239977c23dee578a4cccb418
Nato4ka99 <Nataronska@gmail.com>
2018-11-09 20:51:43 +0200

    added class

--- src/main/java/com/women/online/shop/entities/Order.java
+++ src/main/java/com/women/online/shop/entities/Order.java
@@ -4,7 +4,7 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Order {
+public class Order extends AbstractEntity {
 
     private List<Product> items = new ArrayList<>();
     private double orderAmount;
